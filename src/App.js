a517525b67213d675f6a2b00501980195f25d29e
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2022-02-20 11:33:51 -0600

    set up sign in

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Calculator from './Calculator';
 import Checklist from './Checklist';
+import SignIn from './SignIn'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Checklist>
-        </Checklist>
+        <SignIn>
+
+        </SignIn>
       </header>
     </div>
   );
