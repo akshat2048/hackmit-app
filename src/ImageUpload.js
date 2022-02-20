ad3e9ed735a1174096d783c7a97ec2d6fab8c7d0
24parida <68526448+24parida@users.noreply.github.com>
2022-02-20 12:16:24 -0600

    Update ImageUpload.js

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -3,37 +3,17 @@ import React from "react";
 //Card
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import CardMedia from "@material-ui/core/CardMedia";
 
 import Fab from "@material-ui/core/Fab";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import Avatar from "@material-ui/core/Avatar";
 
 import red from "@material-ui/core/colors/red";
-import pink from "@material-ui/core/colors/pink";
 import blue from "@material-ui/core/colors/blue";
 
-import Icon from "@material-ui/core/Icon";
-import PageviewIcon from "@material-ui/icons/Pageview";
-import SearchIcon from "@material-ui/icons/Search";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
-import CollectionsIcon from "@material-ui/icons/Collections";
 
 import Typography from "@material-ui/core/Typography";
-import Popover from "@material-ui/core/Popover";
-
-// Search
-import Paper from "@material-ui/core/Paper";
-import InputBase from "@material-ui/core/InputBase";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import CloseIcon from "@material-ui/icons/Close";
-import ReplayIcon from "@material-ui/icons/Replay";
 
 //Tabs
 import { withStyles } from "@material-ui/core/styles";
@@ -138,18 +118,6 @@ class ImageUploadCard extends React.Component {
     });
   };
 
-  handleSearchClick = event => {
-    this.setState({
-      mainState: "search"
-    });
-  };
-
-  handleGalleryClick = event => {
-    this.setState({
-      mainState: "gallery"
-    });
-  };
-
   renderInitialState() {
     const { classes, theme } = this.props;
     const { value } = this.state;
@@ -181,123 +149,6 @@ class ImageUploadCard extends React.Component {
     );
   }
 
-  handleSearchURL = event => {
-    console.log();
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    var url = reader.readAsDataURL(file);
-
-    reader.onloadend = function(e) {
-      this.setState({
-        selectedFile: [reader.result]
-      });
-    }.bind(this);
-    console.log(url); // Would see a path?
-
-    this.setState({
-      selectedFile: event.target.files[0],
-      imageUploaded: 1
-    });
-  };
-
-  handleImageSearch(url) {
-    var filename = url.substring(url.lastIndexOf("/") + 1);
-    console.log(filename);
-    this.setState({
-      mainState: "uploaded",
-      imageUploaded: true,
-      selectedFile: url,
-      fileReader: undefined,
-      filename: filename
-    });
-  }
-
-  handleSeachClose = event => {
-    this.setState({
-      mainState: "initial"
-    });
-  };
-
-  renderSearchState() {
-    const { classes } = this.props;
-
-    return (
-      <Paper className={classes.searchRoot} elevation={1}>
-        <InputBase className={classes.searchInput} placeholder="Image URL" />
-        <IconButton
-          className={classes.button}
-          aria-label="Search"
-          onClick={this.handleImageSearch}
-        >
-          <SearchIcon />
-        </IconButton>
-        <Divider className={classes.searchDivider} />
-        <IconButton
-          color="primary"
-          className={classes.secondaryButton}
-          aria-label="Close"
-          onClick={this.handleSeachClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      </Paper>
-    );
-  }
-
-  handleAvatarClick(value) {
-    var filename = value.url.substring(value.url.lastIndexOf("/") + 1);
-    console.log(filename);
-    this.setState({
-      mainState: "uploaded",
-      imageUploaded: true,
-      selectedFile: value.url,
-      fileReader: undefined,
-      filename: filename
-    });
-  }
-
-  renderGalleryState() {
-    const { classes } = this.props;
-    const listItems = this.props.imageGallery.map(url => (
-      <div
-        onClick={value => this.handleAvatarClick({ url })}
-        style={{
-          padding: "5px 5px 5px 5px",
-          cursor: "pointer"
-        }}
-      >
-        <Avatar src={url} />
-      </div>
-    ));
-
-    /*const listItems = this.props.imageGallery.map(url => (
-      <div
-        onClick={value => this.handleAvatarClick({ url })}
-        style={{
-          padding: "5px 5px 5px 5px",
-          cursor: "pointer"
-        }}
-      >
-        <Avatar shape="square" size={100} src={url} />
-      </div>
-    ));*/
-
-    return (
-      <React.Fragment>
-        <Grid>
-          {listItems}
-          <IconButton
-            color="primary"
-            className={classes.secondaryButton}
-            aria-label="Close"
-            onClick={this.handleSeachClose}
-          >
-            <ReplayIcon />
-          </IconButton>
-        </Grid>
-      </React.Fragment>
-    );
-  }
 
   renderUploadedState() {
     const { classes, theme } = this.props;
