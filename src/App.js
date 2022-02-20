0fecb2fa87f69501eb7ffb49c25687817126f42e
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2022-02-20 12:07:30 -0600

    set up pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Calculator from './Calculator';
 import Checklist from './Checklist';
 import SignIn from './SignIn'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SignIn>
+        <Router>
+          <Switch>
+            <Route path='/checklist'>
+              <Checklist />
+            </Route>
+            <Route path='/signin'>
+              <SignIn />
+            </Route>
+            <Route path='/'>
+              <Calculator />
+            </Route>
+          </Switch>
+        </Router>
 
-        </SignIn>
       </header>
     </div>
   );
